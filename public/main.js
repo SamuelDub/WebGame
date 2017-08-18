660ac077e8e3d74d4653a1bd13b1bd0999ef173a
SamuelDub <dubnersamuel@yahoo.com>
2017-08-18 15:36:45 -0700

    stuff

--- public/main.js
+++ public/main.js
@@ -1,89 +1,108 @@
-var myGamePiece;
+var canvas = document.getElementById("ctx")
+var ctx = canvas.getContext("2d");
 
-function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
-    myGameArea.start();
+var player = {
+    startX: canvas.width / 2,
+    startY: canvas.height / 2,
+    radius: 10,
+    name: "something"
 }
 
-var myGameArea = {
-    canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = 1280;
-        this.canvas.height = 720;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateGameArea, 20);
-    },
-    clear: function () {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+var speed;speed
+var allCircles = []
+
+var startCircleName = prompt("What is the name of your circle?");
+var startCircle = {
+    x: player.startX,
+    y: player.startY,
+    radius: player.radius,
+    name: startCircleName
 }
 
-function component(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
-    this.x = x;
-    this.y = y;
-    this.update = function () {
-        ctx = myGameArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function () {
-        this.x += this.speedX;
-        this.y += this.speedY;
-    }
+allCircles.push(startCircle)
+
+function makeCircle() {}
+
+// Draw circle.
+function drawCircles() {
+    allCircles.forEach(function (circle) {
+        ctx.fillStyle = "#FFFFFF";
+        ctx.beginPath();
+        ctx.arc(circle.x, circle.y, circle.radius, 0, Math.PI * 2, true);
+        ctx.fillStyle = "#07C";
+        ctx.fill();
+    });
 }
 
-function updateGameArea() {
-    myGameArea.clear();
-    myGamePiece.newPos();
-    myGamePiece.update();
+var UP = false;
+var DOWN = false;
+var LEFT = false;
+var RIGHT = false;
+
+function changePosition(x, y) {
+   for (var i in allCircles) {
+     if (allCircles[i].name == startCircleName) {
+        allCircles[i].x = x;
+        allCircles[i].y = y;
+        console.log("changed")
+        break;
+     }
+   }
 }
 
-function moveup() {
-    myGamePiece.speedY -= 1;
-}
-
-function movedown() {
-    myGamePiece.speedY += 1;
-}
-
-function moveleft() {
-    myGamePiece.speedX -= 1;
-}
-
-function moveright() {
-    myGamePiece.speedX += 1;
-}
-
-document.onkeydown = function (event) {
-    console.log("working")
-
-    var left = true;
-    var right = true;
-    var up = true;
-    var down = true;
-
-    if (myGamePiece.speedX == 2) {
-        right = false;
-    } else if (myGamePiece.speedX == -2) {
-        left = false;
-    } else if (myGamePiece.speedY == 2) {
-        down = false;
-    } else if (myGamePiece.speedY == -2) {
-        up = false;
+function move() {
+    console.log(startCircle)
+    console.log(startCircle.x)
+    console.log(startCircle.y)
+    if (LEFT && startCircle.x > 10) {
+        console.log("moved")
+        startCircle.x -= speed;
+        changePosition(startCircle.x, startCircle.y)
+    } else
+    if (RIGHT && startCircle.x < 1268) {
+        console.log("moved")
+        startCircle.x += speed;
+        changePosition(startCircle.x, startCircle.y)
+    } else
+    if (UP && startCircle.y > 12) {
+        console.log("moved")
+        startCircle.y -= speed;
+        changePosition(startCircle.x, startCircle.y)
+    } else
+    if (DOWN && startCircle.y < 710) {
+        console.log("moved")
+        startCircle.y += speed;
+        changePosition(startCircle.x, startCircle.y)
     }
 
-    if (event.keyCode == 39 && right == true) {
-        moveright();
-    } else if (event.keyCode == 38 && up == true) {
-        moveup();
-    } else if (event.keyCode == 40 && down == true) {
-        movedown();
-    } else if (event.keyCode == 37 && left == true) {
-        moveleft();
-    }
+}
+
+document.onkeydown = function (e) {
+    if (e.keyCode == 37) LEFT = true;
+    if (e.keyCode == 39) RIGHT = true;
+    if (e.keyCode == 38) UP = true;
+    if (e.keyCode == 40) DOWN = true;
+    // if (e.keyCode == 13) {
+    //     makeCircle()
+    //     console.log("drew circle")
+    // }
+}
+
+document.onkeyup = function (e) {
+    if (e.keyCode == 37) LEFT = false;
+    if (e.keyCode == 39) RIGHT = false;
+    if (e.keyCode == 38) UP = false;
+    if (e.keyCode == 40) DOWN = false;
+}
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+setInterval(update, 10);
+
+function update() {
+    clearCanvas();
+    drawCircles();
+    move();
 }
